8ecff8bf50958d9df26b0383fb1ff9cd4a5f89ed
baileynance <baieyj.nance@gmail.com>
2026-01-22 23:11:23 -0600

    added tailwind and postcss to project. Changed config files, postcss.config to .cjs instead of .js to be compatible with tailwind v4

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,8 +1,14 @@
+import './styles.css';
 import Board from "./components/Board.jsx";
 
 export default function App() {
   return (
-    <Board />
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-600 p-4">
+        Hello world!
+      </h1>
+      <Board />
+    </>
   )
 }
 
