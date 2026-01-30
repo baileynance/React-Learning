cdfd97ed4c2b47eb2d9bdb0815e16bbc257195cf
baileynance <baieyj.nance@gmail.com>
2026-01-29 23:39:19 -0600

    added passing handleclick prop to square component to update when square is clicked component reflects X

--- tic-tac-toe/src/components/Square.jsx
+++ tic-tac-toe/src/components/Square.jsx
@@ -1,5 +1,5 @@
-export default function Square({ value }) {
+export default function Square({ value, onSquareClick }) {
 
-  return <button className="w-20 h-20 m-1 bg-white border rounded-lg border-gray-400 hover:bg-gray-200">{value}</button>;
+  return <button onClick={onSquareClick} className="w-20 h-20 m-1 bg-white border rounded-lg border-gray-400 hover:bg-gray-200">{value}</button>;
 
 }
