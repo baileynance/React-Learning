3f4b7dd01460cab02c13c9787a7a93d2b393765d
baileynance <baieyj.nance@gmail.com>
2026-02-03 19:55:59 -0600

    added div to center above the grid. Transition when onclick is handled

--- tic-tac-toe/src/components/Board.jsx
+++ tic-tac-toe/src/components/Board.jsx
@@ -36,18 +36,23 @@ export default function Board() {
   
   return (
     <>
-      <div className="status">{status}</div>
       <div className="grid place-items-center min-h-screen">
-        <div className="grid grid-cols-3 gap-y-0">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+        <div className="flex flex-col items-center">
+          <div className="status mb-4 text-xl font-semibold">
+            {status}
+          </div>
+
+          <div className="grid grid-cols-3 gap-y-0">
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          </div>
         </div>
       </div>
     </>
